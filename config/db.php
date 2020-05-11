b613fdefff2ba12a18d3034ac96ad02968ee7d0d
bezedache29 <bezedache29@gmail.com>
2020-05-11 23:05:21 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,6 +1,5 @@
 <?php
     $db = new PDO('mysql:host=localhost;dbname=personnages;charset=utf8', 'root', '');
-    //$db = new PDO('mysql:host=db5000437920.hosting-data.io;dbname=dbs418657;charset=utf8', 'dbu231251', '!Gigi!29');
     $manager = new PersonnagesManager($db);
     
 ?>
